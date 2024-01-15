40802fd692b572f1b4d001a6660fc728342a9628
junpakPark <112045553+junpakPark@users.noreply.github.com>
2024-01-15 17:04:44 +0900

    refactor: Comments 리팩터링

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,11 +1,11 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 
 const COMMENT_ATTRIBUTES = {
     src: 'https://giscus.app/client.js',
     'data-repo': 'junpakPark/junpakPark.github.io',
     'data-repo-id': 'R_kgDOLBfvfQ',
-    'data-category': 'Show and tell',
-    'data-category-id': 'DIC_kwDOLBfvfc4CcUc-',
+    'data-category': 'General',
+    'data-category-id': 'DIC_kwDOLBfvfc4CcUc7',
     'data-mapping': 'pathname',
     'data-strict': '1',
     'data-reactions-enabled': '1',
@@ -18,16 +18,24 @@ const COMMENT_ATTRIBUTES = {
 };
 
 const Comments: React.FC = () => {
-
+    const ref = useRef<HTMLDivElement>(null);
     const appendGiscusScript = () => {
-        const giscusContainer = document.querySelector('#giscus');
-        if (!giscusContainer) return;
+        const currentRef = ref.current;
+        if (!currentRef || currentRef.hasChildNodes()) return;
 
-        const script = document.createElement('script');
+        const scriptElement = document.createElement('script');
         Object.entries(COMMENT_ATTRIBUTES).forEach(([key, value]) => {
-            script.setAttribute(key, value);
+            scriptElement.setAttribute(key, value);
         });
-        giscusContainer.appendChild(script);
+
+        currentRef.appendChild(scriptElement);
+
+        // 컴포넌트 언마운트 시 스크립트 제거를 위한 클린업 함수
+        return () => {
+            if (currentRef) {
+                currentRef.removeChild(scriptElement);
+            }
+        };
     }
 
     useEffect(() => {
@@ -36,7 +44,7 @@ const Comments: React.FC = () => {
 
     return (
         <section
-            id="giscus"
+            ref={ref}
             className="border-t mt-8 pt-16 min-h-[372px]"
         ></section>
     );

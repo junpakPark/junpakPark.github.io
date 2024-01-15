4f995d77929176ec2008f7c584f667189de1688f
junpakPark <112045553+junpakPark@users.noreply.github.com>
2024-01-15 18:38:48 +0900

    chore: github action nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,6 @@ import rehypeExternalLinks from "rehype-external-links";
 export default defineConfig({
     site: 'https://junpakPark.github.io',
     output: 'static', // 추가한 설정
-    outDir: './docs', // docs 폴더로 정적 파일 배치
     integrations: [
         react(),
         sitemap(),
